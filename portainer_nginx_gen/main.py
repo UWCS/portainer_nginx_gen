4b31b46a5b9b3385434fe25f5db79e64339f31bd
Skiros Habib <skiros@skiros.me>
2023-01-18 13:13:42 +0000

    Try to use portainer api

--- portainer_nginx_gen/main.py
+++ portainer_nginx_gen/main.py
@@ -2,15 +2,25 @@ import os
 from pprint import pprint as pp
 
 import docker
+import requests
+
+API_KEY = os.environ.get("PORTAINER_API_KEY")
+PORTAINER_API_URL = (
+    "https://portainer.uwcs.co.uk/api/endpoints/4/docker/containers/{}/json"
+)
+
+session = requests.session()
+session.headers.update({"X-API-KEY": API_KEY})
 
 client = docker.DockerClient(
     base_url=os.environ.get("DOCKER_HOST", "unix://var/run/docker.sock")
 )
 
 for container in client.containers.list():
-    print("*" * 40)
-    pp(container.attrs)
-    print("*" * 40)
+    container_id = container.id
+    data = session.get(PORTAINER_API_URL.format(container_id))
+    print(data.text())
+
 
 for event in client.events(decode=True):
     print("*" * 40)
